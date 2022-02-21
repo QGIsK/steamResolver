4a2de69404853357be743f5be025f9554f6ff683
Demian <devaccdemiann@gmail.com>
2022-02-21 15:56:12 +0100

    chore: update analytics

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -6,8 +6,8 @@ export default theme({
             {
                 async: true,
                 defer: true,
-                'data-website-id': '381b60e5-15bc-42e2-8b78-22f93e55b8c9',
-                src: 'https://media.demiann.dev/umami.js',
+                'data-website': 'steamresolver.docs.demiann.dev',
+                src: 'https://media.demiann.dev/js/script.js',
             },
         ],
     },
